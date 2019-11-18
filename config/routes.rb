7ec1b9bb858079e20ca69ece5e60d80c9b2b982d
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-18 18:33:41 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,43 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :profile, only: :show
+
+  resources :friendships, only: [:index, :new, :create] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+
+  resource :dashboard, only: :show
+
+  # PROGRAMS
+
+  resources :programs, only: [:index, :show] do
+    resources :tasks,         only: [:show] do
+    resources :user_programs, only: [:create]
+  end
+
+  resources :user_tasks, only: [] do
+    member do
+      patch :done
+    end
+  end
+
+  # CHALLENGES
+
+  resources :challenges, only: [:index, :show] do
+    resources :user_challenges, only: [:create]
+  end
+
+  resources :challengers, only: [] do
+    member do
+      patch :accept
+      patch :decline
+      patch :abort
+    end
+  end
 end
