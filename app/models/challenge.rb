eebf60d790bc85e42880fb95ad1fd5da068eb673
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-25 10:58:45 +0100

    adding icon column to challenges table

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,6 @@
 class Challenge < ApplicationRecord
   has_many :user_challenges
   has_many :challengers, through: :user_challenges
-
+  mount_uploader :icon, PhotoUploader
   mount_uploader :picture, PhotoUploader
 end
