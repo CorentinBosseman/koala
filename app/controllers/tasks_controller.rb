91270da374015434e75223b8a4b8fdab95368a1d
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-26 17:34:44 +0100

    icon is shit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   def show
-    @task      = Task.find(params[:id])
+    @task = Task.find(params[:id])
+    @program = Program.find(params[:program_id])
     @user_task = UserTask.joins(task: :program, user_program: :user).
       find_by(user_programs: { user: current_user, program: @task.program }, task: @task)
   end
