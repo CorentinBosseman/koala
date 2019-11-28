42f1152061fa39b0b1a6dead2cc52b811d94552c
Remi Beucherie <beucherie.r@gmail.com>
2019-11-28 12:57:05 +0100

    add lvl

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -3,26 +3,29 @@ class UserTasksController < ApplicationController
     @user_task = UserTask.find(params[:id])
     program = @user_task.task.program
 
-    @user_task.completed = true
-    current_user.update_attributes(points: (current_user.points + 20))
-    @user_task.save
-
+    @user_task.update(completed: true)
+    current_user.update(points: (current_user.points + 20))
+    recalculate_level!
     next_task = Task.find_by(program: program, position: @user_task.task.position + 1)
 
     if next_task
       UserTask.create(user_program: @user_task.user_program, task: next_task)
     else
       @user_task.user_program.update(completed: true)
-      if program.position == 1
-        current_user.update_attributes(points: (current_user.points + 50))
-      elsif program.position == 2
-        current_user.update_attributes(points: (current_user.points + 80))
-      elsif program.position == 3
-        current_user.update_attributes(points: (current_user.points + 130))
-      end
 
+      if program.position == 1
+        current_user.update(points: (current_user.points + 50))
+      elsif program.position == 2
+        current_user.update(points: (current_user.points + 80))
+      elsif program.position == 3
+        current_user.update(points: (current_user.points + 130))
+      end
     end
 
     redirect_to program_path(program)
+  end
+
+  def recalculate_level!
+    current_user.update(level: User.level_from_points(current_user.points))
   end
 end
