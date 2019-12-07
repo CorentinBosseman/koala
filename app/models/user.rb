5f49d8df930955f38039381895e7137da4616acf
Remi Beucherie <beucherie.r@gmail.com>
2019-12-07 11:29:04 +0100

    devise setup for pseudo and mail uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, PhotoUploader
 
+  validates :email, uniqueness: true
+  validates :pseudo, uniqueness: true
+
   def friends
     relationships = Friendship.where("user_id = :id OR friend_id = :id", id: id).where(status: "Accepted")
     friend_ids    = relationships.map(&:user_id) + relationships.map(&:friend_id)
